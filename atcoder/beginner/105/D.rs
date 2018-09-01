6999ed32bfe120bad2280586b8a620ea6d8be1c4
care0717 <dev.care0717@gmail.com>
2018-09-01 17:26:27 +0900

    feat: 105といた

--- atcoder/beginner/105/D.rs
+++ atcoder/beginner/105/D.rs
@@ -1,10 +1,11 @@
+use std::collections::HashMap;
 macro_rules! input {
     (source = $s:expr, $($r:tt)*) => {
         let mut iter = $s.split_whitespace();
         input_inner!{iter, $($r)*}
     };
     ($($r:tt)*) => {
-        let mut s = {
+        let s = {
             use std::io::Read;
             let mut s = String::new();
             std::io::stdin().read_to_string(&mut s).unwrap();
@@ -47,8 +48,43 @@ macro_rules! read_value {
     };
 }
 
+fn comb(n: i64, r: i64) -> i64 {
+    match (n, r) {
+        (0, _) | (_, 0) => 1,
+        _ => comb(n, r - 1) * (n - r + 1) / r
+    }
+}
+
+
 fn main() {
     input!{
         n: usize,
+        m: i64,
+        v: [i64; n],
     }
+    let mut sum: Vec<i64> = Vec::new();
+    sum.push(v[0] % m);
+    let mut temp = v[0] % m ;
+    for i in 0..n-1 {
+        temp += v[i+1];
+        sum.push(temp % m);
+    }
+    let mut map: HashMap<i64, i64>  = HashMap::new();
+
+    for x in sum.iter() {
+        if !map.contains_key(&x) {
+            map.insert(*x, 1);
+        } else {
+            *map.get_mut(&x).unwrap() += 1;
+        }
+    }
+    let mut res: i64= 0;
+    for (key, val) in map.iter() {
+        if  *val > 1 {
+            res += comb(*val, 2_i64)
+        }
+    }
+    res += *map.get(&0).unwrap_or(&0);
+    println!("{}", res);
+
 }
