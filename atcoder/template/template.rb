c156a18c35c7dc07096d12ee4309cb56427a6ef7
care0717 <dev.care0717@gmail.com>
2018-09-01 15:13:59 +0900

    feat: templateを追加

--- atcoder/template/template.rb
+++ atcoder/template/template.rb
@@ -12,6 +12,15 @@ def ili()
   as = gets.split.map(&:to_i)
   [n, as]
 end
+class Array
+  def sum
+    total = 0
+    self.each do |item|
+      total += item
+    end
+    total
+  end
+end
 
 def eratosthenes(size)
   prime_list = []
