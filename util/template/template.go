a6eabc001d72fadaf7d0d6bd14e570cf474326d4
care0717 <dev.care0717@gmail.com>
2020-05-02 20:23:15 +0900

    feat: update template

--- util/template/template.go
+++ util/template/template.go
@@ -2,36 +2,49 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"io"
 	"os"
 	"strconv"
-	"strings"
-	"fmt"
 )
 
-var sc = bufio.NewScanner(os.Stdin)
+var (
+	// ReadString returns a WORD string.
+	ReadString func() string
+)
 
-func read() string {
-	sc.Scan()
-	return sc.Text()
+func init() {
+	ReadString = newReadString(os.Stdin, bufio.ScanWords)
 }
 
-func geti() int {
-	n, _ := strconv.Atoi(read())
+func newReadString(ior io.Reader, sf bufio.SplitFunc) func() string {
+	r := bufio.NewScanner(ior)
+	r.Buffer(make([]byte, 1024), int(1e+9)) // for Codeforces
+	r.Split(sf)
+
+	return func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+}
+
+func readInt() int {
+	n, _ := strconv.Atoi(ReadString())
 	return n
 }
 
 // 10 11 12 => [10, 11, 12]
-func getli(size int) ([]int) {
+func readIntSlice(size int) []int {
 	a := make([]int, size)
-	list := strings.Split(read(), " ")
-	for i, s := range list {
-		n, _ := strconv.Atoi(s)
-		a[i] = n
+	for i := 0; i < size; i++ {
+		a[i] = readInt()
 	}
 	return a
 }
 
-func get2byte(size int) ([][]byte) {
+func get2byte(size int) [][]byte {
 	a := make([][]byte, size)
 	for i := 0; i < size; i++ {
 		var low string
@@ -48,6 +61,62 @@ func transpose(a [][]int) {
 			a[i][j], a[j][i] = a[j][i], a[i][j]
 		}
 	}
+}
+
+func fact(n, m int) int {
+	res := 1
+	for i := m + 1; i <= n; i++ {
+		res *= i
+	}
+	return res
+}
+
+func perm(n, r int) int {
+	if r > n {
+		return 0
+	}
+	return fact(n, n-r)
+}
+
+func comb(n, m int) int {
+	if m > n {
+		return 0
+	}
+	return fact(n, n-m) / fact(m, 0)
+}
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func min(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m > integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+func max(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m < integer {
+			m = integer
+		}
+	}
+	return m
 }
 
 func main() {
