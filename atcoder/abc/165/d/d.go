e437d55e4492522b6394355b33d8f9da08bab270
care0717 <dev.care0717@gmail.com>
2020-05-03 19:23:11 +0900

    feat: add abc165

---
+++ atcoder/abc/165/d/d.go
@@ -0,0 +1,135 @@
+package d
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+var (
+	// ReadString returns a WORD string.
+	ReadString func() string
+)
+
+func init() {
+	ReadString = newReadString(os.Stdin, bufio.ScanWords)
+}
+
+func newReadString(ior io.Reader, sf bufio.SplitFunc) func() string {
+	r := bufio.NewScanner(ior)
+	r.Buffer(make([]byte, 1024), int(1e+9)) // for Codeforces
+	r.Split(sf)
+
+	return func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+}
+
+func readInt() int {
+	n, _ := strconv.Atoi(ReadString())
+	return n
+}
+
+func readInt64() int64 {
+	n, _ := strconv.ParseInt(ReadString(), 0, 64)
+	return n
+}
+
+// 10 11 12 => [10, 11, 12]
+func readIntSlice(size int) []int {
+	a := make([]int, size)
+	for i := 0; i < size; i++ {
+		a[i] = readInt()
+	}
+	return a
+}
+
+func get2byte(size int) [][]byte {
+	a := make([][]byte, size)
+	for i := 0; i < size; i++ {
+		var low string
+		fmt.Scan(&low)
+		a[i] = []byte(low)
+	}
+	return a
+}
+
+func transpose(a [][]int) {
+	n := len(a)
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			a[i][j], a[j][i] = a[j][i], a[i][j]
+		}
+	}
+}
+
+func fact(n, m int) int {
+	res := 1
+	for i := m + 1; i <= n; i++ {
+		res *= i
+	}
+	return res
+}
+
+func perm(n, r int) int {
+	if r > n {
+		return 0
+	}
+	return fact(n, n-r)
+}
+
+func comb(n, m int) int {
+	if m > n {
+		return 0
+	}
+	return fact(n, n-m) / fact(m, 0)
+}
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func min(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m > integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+func max(integers ...int) int {
+	m := integers[0]
+	for i, integer := range integers {
+		if i == 0 {
+			continue
+		}
+		if m < integer {
+			m = integer
+		}
+	}
+	return m
+}
+
+func main() {
+	a, b, n := readInt64(), readInt64(), readInt64()
+	if b-1 >= n {
+		fmt.Println(int64(math.Floor(float64(a)*float64(n)/float64(b)) - float64(a)*math.Floor(float64(n)/float64(b))))
+	} else {
+		fmt.Println(int64(math.Floor(float64(a)*float64(b-1)/float64(b)) - float64(a)*math.Floor(float64(b-1)/float64(b))))
+	}
+}
